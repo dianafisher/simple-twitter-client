65453937069ed66ab37a094c0d8e146c3a278cd6
Diana Fisher <diana.fisher@gmail.com>
2017-09-25 11:19:31 -0700

    Initial Commit

---
+++ SimpleTwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTwitterClient
+//
+//  Created by Diana Fisher on 9/25/17.
+//  Copyright © 2017 Diana Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
