64c57b7ad2858974ef3f90b49fd3397772905519
Diana Fisher <diana.fisher@gmail.com>
2017-09-26 13:47:06 -0700

    Add twitter logo to launch screen.

--- SimpleTwitterClient/AppDelegate.swift
+++ SimpleTwitterClient/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    // twitter blue = #1DA1F2
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
