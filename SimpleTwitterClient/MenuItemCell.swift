27e9a8cad5b3bbb7dacfb650f3173118e03066c6
Diana Fisher <diana.fisher@gmail.com>
2017-10-03 22:16:36 -0700

    Update gitignore

---
+++ SimpleTwitterClient/MenuItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuItemCell.swift
+//  SimpleTwitterClient
+//
+//  Created by Diana Fisher on 10/3/17.
+//  Copyright © 2017 Diana Fisher. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
